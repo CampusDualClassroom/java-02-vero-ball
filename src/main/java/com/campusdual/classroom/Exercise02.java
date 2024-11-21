46a1b6a7b6079100a5d090c6219806bbf5046164
vero-ball <v.rivadulla.deschamps@gmail.com>
2024-11-21 12:52:24 +0100

    ej2

--- src/main/java/com/campusdual/classroom/Exercise02.java
+++ src/main/java/com/campusdual/classroom/Exercise02.java
@@ -7,11 +7,11 @@ public class Exercise02 {
         double a, l;
 
         // TODO: ↓ Fórmula del área de un círculoS: a = radio * radio * Número PI
-
+        a = r*r*Math.PI;
         System.out.println("Area: " + a);
 
         // TODO: ↓ Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
-
+        l = 2*r*Math.PI;
         System.out.println("Longitud:  " + l);
 
     }
